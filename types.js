73ffc93207894977ea9fd9f970c5edeb5c41b465
s30408 <45545124+s30408@users.noreply.github.com>
2019-03-15 08:41:50 +0900

    first ver.

---
+++ types.js
@@ -0,0 +1,64 @@
+var types = {};
+types.type1 = {
+	"name" : "type1",
+	"width" : 2,
+	"height" : 2,
+	"maxRotation" : 1,
+	"shape" : 	[[1,1],
+        			 [1,1]]
+};
+types.type2 = {
+	"name" : "type2",
+	"width" : 3,
+	"height" : 3,
+	"maxRotation" : 4,
+	"shape" : 	[[0,1,0],
+        			 [1,1,1],
+        			 [0,0,0]]
+};
+types.type3 = {
+	"name" : "type3",
+	"width" : 4,
+	"height" : 4,
+	"maxRotation" : 2,
+	"shape" : 	[[0,1,0,0],
+        			 [0,1,0,0],
+        			 [0,1,0,0],
+        			 [0,1,0,0]]
+};
+types.type4 = {
+	"name" : "type4",
+	"width" : 3,
+	"height" : 3,
+	"maxRotation" : 4,
+	"shape" : 	[[0,1,0],
+        			 [0,1,0],
+        			 [0,1,1]]
+};
+types.type5 = {
+	"name" : "type5",
+	"width" : 3,
+	"height" : 3,
+	"maxRotation" : 4,
+	"shape" : 	[[0,1,0],
+        			 [0,1,0],
+        			 [1,1,0]]
+};
+types.type6 = {
+	"name" : "type6",
+	"width" : 3,
+	"height" : 3,
+	"maxRotation" : 2,
+	"shape" : 	[[1,0,0],
+        			 [1,1,0],
+        			 [0,1,0]]
+};
+types.type7 = {
+	"name" : "type7",
+	"width" : 3,
+	"height" : 3,
+	"maxRotation" : 2,
+	"shape" : 	[[0,1,0],
+        			 [1,1,0],
+        			 [1,0,0]]
+};
